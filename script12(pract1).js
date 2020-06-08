c0846380ba6a0d6139df03a47a6386c3405373bd
Den <pd8686@mail.ru>
2020-06-08 19:38:55 +0400

    add script12(pract1).js tasc 2

--- script12(pract1).js
+++ script12(pract1).js
@@ -2,3 +2,12 @@
 
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '1');
 console.log(numberOfFilms);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: obj1 = {},
+    actors: obj2 = {},
+    genres: mas = [],
+    privat: false
+};
+
