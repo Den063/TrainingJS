94b5cc9c6bff8fd2401fdc4252d3930a3f98c565
Den <pd8686@mail.ru>
2020-06-08 21:21:41 +0400

    script12(pract1).js tasc 3

--- script12(pract1).js
+++ script12(pract1).js
@@ -1,13 +1,28 @@
 "use strict";
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '1');
-console.log(numberOfFilms);
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '1');
+//console.log(numberOfFilms);
 
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: obj1 = {},
-    actors: obj2 = {},
-    genres: mas = [],
+    movies: {},
+    actors: {},
+    genres: [],
     privat: false
 };
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', '1'),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '1');
+      
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
+
+
 
